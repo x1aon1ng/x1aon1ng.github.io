c400793d004fc9f50e8e2876fddb6ce2ffd9f9ad
x1aon1ng <3049155267@qq.com>
2024-05-25 13:32:03 +0800

    Site updated: 2024-05-25 13:32:02

--- js/search.js
+++ js/search.js
@@ -1,151 +1,147 @@
-(function () {
-  var searchWord = document.getElementById("search-key"),
-    searchLocal = document.getElementById("search-local"),
-    searchForm = document.getElementById("search-form"),
-    searchMask = document.getElementById("result-mask"),
-    searchWrap = document.getElementById("result-wrap"),
-    searchResult = document.getElementById("search-result"),
-    searchTpl = document.getElementById("search-tpl").innerHTML,
-    winWidth,
-    winHeight,
-    searchData;
-  if (window.innerWidth) {
-    winWidth = parseInt(window.innerWidth);
-  } else if (document.body && document.body.clientWidth) {
-    winWidth = parseInt(document.body.clientWidth);
-  }
-  if (window.innerHeight) {
-    winHeight = parseInt(window.innerHeight);
-  } else if (document.body && document.body.clientHeight) {
-    winHeight = parseInt(document.body.clientHeight);
-  }
-  searchMask.style.width = winWidth + "px";
-  searchMask.style.height = winHeight + "px";
-
-  function tpl(html, data) {
-    return html.replace(/\{\w+\}/g, function (str) {
-      var prop = str.replace(/\{|\}/g, "");
-      return data[prop] || "";
-    });
-  }
-
-  function hasClass(obj, cls) {
-    return obj.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"));
-  }
-
-  function addClass(obj, cls) {
-    if (!hasClass(obj, cls)) obj.className += " " + cls;
-  }
-
-  function removeClass(obj, cls) {
-    if (hasClass(obj, cls)) {
-      var reg = new RegExp("(\\s|^)" + cls + "(\\s|$)");
-      obj.className = obj.className.replace(reg, " ");
-    }
-  }
-
-  function matcher(post, regExp) {
-    return regtest(post.title, regExp) || regtest(post.text, regExp);
-  }
-
-  function regtest(raw, regExp) {
-    regExp.lastIndex = 0;
-    return regExp.test(raw);
-  }
-
-  function searchShow() {
-    removeClass(searchWrap, "hide");
-    removeClass(searchMask, "hide");
-  }
-
-  function searchHide() {
-    addClass(searchWrap, "hide");
-    addClass(searchMask, "hide");
-  }
-
-  function loadData(success) {
-    if (!searchData) {
-      var xhr = new XMLHttpRequest();
-      xhr.open(
-        "GET",
-        document.getElementsByTagName("meta")["root"].content + "content.json",
-        true
-      );
-      xhr.onload = function () {
-        if (this.status >= 200 && this.status < 300) {
-          var res = JSON.parse(this.response || this.responseText);
-          searchData = res instanceof Array ? res : res.posts;
-          success(searchData);
-        } else {
-          console.error(this.statusText);
+// A local search script with the help of [hexo-generator-search](https://github.com/PaicHyperionDev/hexo-generator-search)
+// Copyright (C) 2015 
+// Joseph Pan <http://github.com/wzpan>
+// Shuhao Mao <http://github.com/maoshuhao>
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+// 
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+// 02110-1301 USA
+// 
+var searchAll;
+var searchFunc = function (path, search_id, content_id) {
+  'use strict';
+  $.ajax({
+    url: path,
+    dataType: 'xml',
+    complete: function (xmlResponse) {
+      // get the contents from search data
+        /* 由于 xml 不支持特殊字符，所有手动转 */
+        function createXml(str){
+            if(document.all){
+                var xmlDom=new ActiveXObject("Microsoft.XMLDOM");
+                xmlDom.loadXML(str);
+                return xmlDom;
+            }
+            else
+                return new DOMParser().parseFromString(str, "text/xml");
         }
-      };
-      xhr.onerror = function () {
-        console.error(this.statusText);
-      };
-      xhr.send();
-    } else {
-      success(searchData);
-    }
-  }
+      var datas = $("entry", createXml(xmlResponse.responseText)).map(function () {
+        return {
+          title: $("title", this).text(),
+          content: $("content", this).text(),
+          url: $("url", this).text()
+        };
+      }).get();
 
-  function render(data) {
-    var html = "";
-    if (data.length) {
-      html = data
-        .map(function (post) {
-          return tpl(searchTpl, {
-            title: filter(post.title, "title"),
-            path: post.path,
-            content: filter(post.text, "content"),
+      var $input = document.getElementById(search_id);
+      var $resultContent = document.getElementById(content_id);
+
+      searchAll = function (val) {
+        var str = '<ul class=\"search-result-list\">';
+        var keywords = val.trim().toLowerCase().trim().split(/[\s\-]+/);
+        $resultContent.innerHTML = "";
+        if (val.trim().length <= 0) {
+          return;
+        }
+        // perform local searching
+        datas.forEach(function (data) {
+          var isMatch = true;
+          var content_index = [];
+          if (!data.title || data.title.trim() === '') {
+            data.title = "Untitled";
+          }
+          var data_title = data.title.trim().toLowerCase();
+          var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+          var data_url = data.url;
+          var index_title = -1;
+          var index_content = -1;
+          var first_occur = -1;
+          // only match artiles with not empty contents
+          if (data_content !== '') {
+            keywords.forEach(function (keyword, i) {
+              index_title = data_title.indexOf(keyword);
+              index_content = data_content.indexOf(keyword);
+
+              if (index_title < 0 && index_content < 0) {
+                isMatch = false;
+              } else {
+                if (index_content < 0) {
+                  index_content = 0;
+                }
+                if (i == 0) {
+                  first_occur = index_content;
+                }
+                // content_index.push({index_content:index_content, keyword_len:keyword_len});
+              }
+            });
+          } else {
+            isMatch = false;
+          }
+          // show search results
+          if (isMatch) {
+            var urls = data_url.split("/");
+            var post_date = urls[1]+"/"+urls[2]+"/"+urls[3];
+            str += "<li><a href='" + data_url + "'><span class='post-title' title='"+data_title+"'>" + data_title + "</span><span class='post-date' title='"+post_date+"'>"+post_date+"</span></a>";
+            var content = data.content.trim().replace(/<[^>]+>/g, "");
+            if (first_occur >= 0) {
+              // cut out 100 characters
+              var start = first_occur - 20;
+              var end = first_occur + 80;
+
+              if (start < 0) {
+                start = 0;
+              }
+
+              if (start == 0) {
+                end = 100;
+              }
+
+              if (end > content.length) {
+                end = content.length;
+              }
+
+              var match_content = content.substr(start, end);
+
+              // highlight all keywords
+              keywords.forEach(function (keyword) {
+                var regS = new RegExp(keyword, "gi");
+                match_content = match_content.replace(regS, "<em class=\"search-keyword\">" + keyword + "</em>");
+              });
+
+              str += "<p class=\"search-result\">" + match_content + "...</p>"
+            }
+            str += "</li>";
+          }
+        });
+        str += "</ul>";
+        if (str.indexOf('<li>') === -1) {
+          return $resultContent.innerHTML = "<ul><span class='local-search-empty'>没有找到内容，更换下搜索词试试吧~<span></ul>";
+        }
+        $resultContent.innerHTML = str;
+
+          $(document).pjax('#local-search-result a', '.pjax', {fragment: '.pjax', timeout: 8000});
+          /*鼠标移出文章列表后，去掉文章标题hover样式*/
+          $("#local-search-result a").mouseenter(function (e) {
+              $("#local-search-result a.hover").removeClass("hover");
+              $(this).addClass("hover");
           });
-        })
-        .join("");
-    } else {
-      if (searchWord.value == "") {
-        searchHide();
-      } else {
-        html = '<div class="tips"><p>没有找到相关结果!</p></div>';
+          $("#local-search-result a").mouseleave(function (e) {
+              $(this).removeClass("hover");
+          });
       }
-    }
-    searchResult.innerHTML = html;
-  }
-
-  function filter(art, type) {
-    var keyword = searchWord.value;
-    var index = art.indexOf(keyword);
-    var artRe = art.replace(keyword, "<b>" + keyword + "</b>");
-    if (type == "title") {
-      return artRe;
-    }
-    if (type == "content" && index > 0) {
-      return artRe.substr(index - 15, 45);
-    }
-  }
-
-  function search(e) {
-    var key = this.value.trim();
-    if (!key) {
-      render("");
-      return;
-    }
-    var regExp = new RegExp(key.replace(/[ ]/g, "|"), "gmi");
-    loadData(function (data) {
-      var result = data.filter(function (post) {
-        return matcher(post, regExp);
-      });
-      render(result);
-    });
-    e.preventDefault();
-    searchShow();
-    searchWord.onfocus = function () {
-      searchShow();
-    };
-  }
-  searchWord.onfocus = function () {
-    searchWord.addEventListener("input", search);
-  };
-  searchMask.onclick = function () {
-    searchHide();
-  };
-})();
+    },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+          console.log('文章中出现特殊字符，导致解析xml出现问题，系统自动采用第二方案：进行主动解析！！！ 请检查全文搜索是否有问题，如出现问题，请及时在 https://github.com/yelog/hexo-theme-3-hexo/issues 中提出来，作者会尽快处理！')
+      }
+  });
+}
